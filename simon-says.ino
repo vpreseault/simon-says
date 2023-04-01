d0abd71ea928170eedeb7a20d3db07e92bb271c1
unknown <vincent.preseault@gmail.com>
2023-04-01 14:06:37 -0400

    refactor

--- simon-says.ino
+++ simon-says.ino
@@ -3,21 +3,23 @@ const int rs = 6, en = 7, d4 = 8, d5 = 9, d6 = 10, d7 = 11;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 int simonLeds[100];
-
 int randomLed;
+
 int leds[4] = { 2, 3, 4, 5 };
 int buttons[4] = { 17, 16, 15, 14 };
 int tones[4] = { 440, 523, 659, 784 };
 int buzzerPin = 12;
+
+int buttonsInput[4] = { 0, 0, 0, 0 };
 
 int currentScore = 0;
 int highscore = 0;
 
 int roundDelay = 1000;
 int sequenceDelay = 250;
+int animationDelay = 50;
+int deadzoneDelay = 25; 
 
-int buttonsInput[4] = { 0, 0, 0, 0 };
-int noInputCounter = 0;
 
 void setup() {
   Serial.begin(9600);
@@ -55,7 +57,6 @@ void loop() {
 }
 
 void userInput() {
-  Serial.println("userInput \n");
   for (int i = 0; i <= currentScore; i++) {
     while (arraySum(buttonsInput, 4) == 4) {
       // loop through all buttons and check if they are pressed
@@ -65,7 +66,7 @@ void userInput() {
           digitalWrite(leds[j], HIGH);
           tone(buzzerPin, tones[j]);
 
-          // verify user input if button[j] is pressed
+          // verify if the correct button is pressed
           if (j != simonLeds[i]) {
             waitForNoInput();
             resetOutputs();
@@ -82,26 +83,21 @@ void userInput() {
 }
 
 void waitForNoInput() {
-  Serial.println("waitForNoInput \n");
   while (arraySum(buttonsInput, 4) != 4) {
     for (int j = 0; j < 4; j++) {
       buttonsInput[j] = digitalRead(buttons[j]);
     }
   }
+  delay(deadzoneDelay);
 }
 
 void simonSelect() {
-  Serial.println("simonSelect \n");
   randomLed = random(4);
   simonLeds[currentScore] = randomLed;
 }
 
 void loopSimonSelections() {
-  Serial.println("loopSimonSelections");
   for (int i = 0; i <= currentScore; i++) {
-    Serial.print(simonLeds[i]);
-    Serial.print(", ");
-
     digitalWrite(leds[simonLeds[i]], HIGH);
     tone(buzzerPin, tones[simonLeds[i]]);
     delay(sequenceDelay);
@@ -109,20 +105,15 @@ void loopSimonSelections() {
     noTone(buzzerPin);
     delay(sequenceDelay);
   }
-  Serial.println("\n");
 }
 
 void lost() {
-  Serial.println("lost \n");
   currentScore = 0;
-
   losingAnimation();
 }
 
 void roundWin() {
-  Serial.println("roundWin \n");
   currentScore++;
-
   if (currentScore > highscore) {
     highscore = currentScore;
   }
@@ -131,7 +122,6 @@ void roundWin() {
 }
 
 void updateLCDScores() {
-  Serial.println("updateLCDScores \n");
   lcd.clear();
   lcd.setCursor(0, 0);
   lcd.print("Score: " + String(currentScore));
@@ -140,7 +130,6 @@ void updateLCDScores() {
 }
 
 void resetOutputs() {
-  Serial.println("resetOutputs \n");
   for (int i = 0; i < 4; i++) {
     digitalWrite(leds[i], LOW);
   }
@@ -157,34 +146,30 @@ int arraySum(int array[], int length) {
 
 // ---------- Animations ----------
 void startupAnimation() {
-  Serial.println("startupAnimation \n");
   delay(sequenceDelay);
   for (int i = 0; i < 4; i++) {
     digitalWrite(leds[i], HIGH);
     tone(buzzerPin, tones[i]);
-    delay(50);
+    delay(animationDelay);
     digitalWrite(leds[i], LOW);
     noTone(buzzerPin);
-    delay(50);
+    delay(animationDelay);
   }
 }
 
 void losingAnimation() {
-  Serial.println("losingAnimation \n");
   delay(sequenceDelay);
   for (int x = 0; x < 3; x++) {
     tone(buzzerPin, tones[0]);
     for (int i = 0; i < 4; i++) {
       digitalWrite(leds[i], HIGH);
     }
-
-    delay(50);
+    delay(animationDelay);
 
     for (int i = 0; i < 4; i++) {
       digitalWrite(leds[i], LOW);
     }
-
     noTone(buzzerPin);
-    delay(50);
+    delay(animationDelay);
   }
 }
